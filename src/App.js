35387e0630f79025a1a8ccda6acd930fa423301f
Gustaf Gröning <gustaf.groning.8601@student.uu.se>
2020-05-17 18:23:49 +0200

    ett meddelande

--- src/App.js
+++ src/App.js
@@ -4,22 +4,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+<body>
+  <img class="intro-image" src="https://www.dailyesports.gg/wp-content/uploads/2019/10/League-of-Legends-Senna-Feature.png"/>
+  <div class="firstgrid">
+         <div class="list">
+       <ul>
+    <li> Home </li>
+    <li> Characters </li>
+    <li> About Riot INC </li>
+  </ul>
+      </div>
+    <div class="whatis">
+    <h1> What is League of Legends? </h1>
     </div>
+  </div>
+  
+    <p class="p1"> 
+      League of Legends (LoL) is a multiplayer online battle arena video game developed and published by Riot Games for Microsoft Windows and macOS. Inspired by the Warcraft III: The Frozen Throne mod Defense of the Ancients, the game follows a freemium model and is supported by microtransactions In League of Legends, players assume the role of a "champion" with unique abilities and battle against a team of other player- or computer-controlled champions. The goal is usually to destroy the opposing team's "Nexus", a structure that lies at the heart of a base protected by defensive structures, although other distinct game modes exist as well with varying objectives, rules, and maps. Each League of Legends match is discrete, with all champions starting off relatively weak but increasing in strength by accumulating items and experience over the course of the game. Champions span a variety of roles and blend a variety of fantasy tropes, such as sword and sorcery, steampunk, and Lovecraftian horror. Although the discrete nature of each match prohibits an overarching narrative in-game, the various champions make up a large and ever-evolving fictional universe developed by Riot Games through short stories, comics, cinematics, and books.
+    </p>
+      
+<div class="middletext">
+    <p class="p2">League of Legends is a team-based strategy game where two teams of five powerful champions face off to destroy the other’s base. Choose from over 140 champions to make epic plays, secure kills, and take down towers as you battle your way to victory.
+      </p>
+      
+      <p class="p3">
+      Your team needs to clear at least one lane to get to the enemy Nexus. Blocking your path are defense structures called turrets and inhibitors. Each lane has three turrets and one inhibitor, and each Nexus is guarded by two turrets.
+    </p>
+
+      </div>
+
+<div class="specs">
+  <h2> In-game Info </h2>
+</div>
+   <div class="last-part">
+    <p class="p4"> In between the lanes is the jungle, where neutral monsters and jungle plants reside. The two most important monsters are Baron Nashor and the Drakes. Killing these units grants unique buffs for your team and can also turn the tide of the game.
+
+BARON NASHOR
+Baron Nashor is the most powerful monster in the jungle. Killing Baron grants the slayer’s team bonus attack damage, ability power, empowered recall, and greatly increases the power of nearby minions.
+
+    </p>
+ <p class="p5"> When champions earn a certain amount of experience, they level up and can unlock or strengthen abilities and raise their base stats. Gain experience by killing enemy units and champions, assisting in a kill, and destroying defense structures.
+   DRAKES
+Drakes, or dragons, are powerful monsters that grant unique bonuses depending on the element of the drake your team slays. There are four Elemental Drakes and one Elder Dragon.
+    </p>
+    </div>
+</body>
+  
+ 
   );
 }
 
